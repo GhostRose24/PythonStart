ae247f2e1867d7cfd65863554f64aee73706a1a8
GhostRose24 <melrose24.tt@gmail.com>
2025-05-12 19:32:06 -0500

    Add files via upload

--- Jankenpo.py
+++ Jankenpo.py
@@ -1,6 +1,6 @@
 import random
 #### 04/28/2025 ####
-#I am starting a new project called Jankenpo, which is a rock-paper-scissors game. This is from the Python Course I am taking. I hope to learn more about Python and Informationo Technology/Computer Science along the way. #
+#I am starting a new project called Jankenpo, which is a rock-paper-scissors game. This is from the Python Course I am taking. I hope to learn more about Python and Information Technology/Computer Science along the way. #
 
 
 #libraries
@@ -13,10 +13,14 @@ import random
 ##Lists
 #food = ['pizza', 'sushi', 'tacos']
 #dinner = random.choice(food)
-
+def greeting():
+    return"Hi! Welcome to Jankenpo or Rock, Paper, Scissors!"
 
 def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors):")
+
+    print (greeting()) #call the greeting function
+
+    player_choice =  input( "Enter a choice (rock, paper, scissors):")
 
     Options= ['rock',  'paper', 'scissors']#list of options for computer_choice to select from
 
@@ -26,38 +30,41 @@ def get_choices():
 
     return choices
 
-def greeting():
-    return"Hi! Welcome to Jankenpo or Rock, Paper, Scissors!"
-
 def check_win(player,computer):
    ## print("You chose " + player + ", computer chose: " + computer + '.')
    print(f"You chose:{player}, computer chose:{computer}.") 
+
    if player == computer:
-      print("It's a tie!")
-      return "It's a tie!" 
-   elif player == 'rock':
-    if computer == "scissors":
-         print("Rock smashes scissors! You win!")
-         return "Rock smashes scissors! You win!"
-   elif player == "paper":
-    if computer == "rock":
-         print("Paper covers rock! You win!")
-         return "Paper covers rock! You win!"
-   elif player == "scissors":
-    if computer == "paper":
-        print("Scissors cuts paper! You win!")
-        return "Scissors cuts paper! You win!"
-    else:
-       return "Rock smashes scissors! You lose!"
-    
+      return "It's a tie!"
+   elif player == 'rock' and computer == "scissors":
+       print("Rock smashes scissors! You win!")
+       return 
+   elif player == 'rock' and computer == 'paper':
+       print("Paper covers rock! You lose!")
+       return 
+   elif player == 'paper' and computer == 'rock':
+       print("Paper covers rock! You win!")
+       return 
+   elif player == 'paper' and computer == 'scissors':
+       print("Scissors cuts paper! You lose!")
+       return 
+   elif player == 'Scissors' and computer == 'rock':
+        print("Rock smashes scissors! You lose!")
+        return 
+   elif player == 'Scissors' and computer == 'paper':
+       print("Scissors cuts paper! You win!")
+       return 
+   else:
+       return 'ERROR: Try again...'
+   
+
+
 
 
 #variables
 response = greeting()
 choices = get_choices()
 result = check_win( choices['player'], choices['computer'])
-
-
 
 
 
